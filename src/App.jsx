a5baf32e8fdf35dd5f9961491a109d6265d70b0d
neerrajpanchal004 <npvishwakarma.143@gmail.com>
2024-05-11 13:43:14 +0530

    json_server

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect,useState } from 'react'
 
 const App = () => {
-  const [data, setdata] = useState([])
-  const port = import.meta.env.VITE_PORT;
+const port = import.meta.env.VITE_PORT;
+const [data, setData] = useState([])
 
-  async function fetchData() {
+  async function fetchData(){
     try {
-      let res = await fetch(port);
-      let data = await res.json();
-      setdata(data)
-  
+      let res = await fetch(`${port}/products`)
+      let data = await res.json()
+      setData(data)
 
+      console.log(data)
     } catch (error) {
       console.log(error)
     }
-
   }
-  useEffect(() => {
+ 
+  useEffect(()=>{
     fetchData()
-  }, [])
+  },[])
+  
   if(data.length==0){
-    return <h1>LOADING</h1>
+    return <h1>Loading...</h1>
   }
-
   return (
-
-    <div className='text-3xl text-red-800 my-52'>
-      {data.map((pro)=>{
-        return <div>
-          <h1>{pro.name}</h1>
-          <h1>{pro.price}</h1>
+    <>
+    <h1 className='text-center mt-20'>JSON-SERVER</h1>
+    <div className='flex m-10 p-10 justify-center gap-10'>
+      
+      {data.map((product)=>{
+        const {id,price,image,name}= product;
+        return <div key={id} className='border-2 border-black p-5'>
+          <h1>{name}</h1>
+          <img src={image} alt={name} className='w-32 h-32 object-cover'/>
+          <p>{name}</p>
+          <p>{price}</p>
         </div>
       })}
     </div>
+    </>
   )
 }
 
